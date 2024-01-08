d426e80253db2db704d57c3a9e62a1699f1ed400
GavrikGal <gavrik.gal@gmail.com>
2024-01-09 01:09:23 +0300

    UT. Репозитория пользователя

--- app/repositories/tests/test_user_repository.py
+++ app/repositories/tests/test_user_repository.py
@@ -1,39 +1,66 @@
-import unittest
+import pytest
 
 from app.repositories.user_repository import UserRepository
-from app.db.database import async_session_maker
+from app.db.database import async_session_maker, engine, Base
+from app.api.schemas.user import UserFromDB
 
 
-@unittest.skip
-class TestUserRepository(unittest.IsolatedAsyncioTestCase):
+@pytest.fixture(scope='function', autouse=True)
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope='module')
+def anyio_backend():
+    return 'asyncio'
+
+
+@pytest.fixture(scope='module', autouse=True)
+async def session():
+    async with async_session_maker() as session:
+        yield session
+    await session.close()
+
+
+class TestUserRepository:
     """ Тесты репозитория пользователей """
 
-    async def asyncSetUp(self):
-        self.session = async_session_maker()
-
-    async def asyncTearDown(self):
-        await self.session.close()
-
-    def test_session_is_available(self):
+    @pytest.mark.anyio
+    async def test_session_is_available(self, session):
         """ Тест наличие сессии в репозитории """
 
-        user_rep = UserRepository(self.session)
+        user_rep = UserRepository(session)
 
-        self.assertIsNotNone(user_rep.session)
+        assert user_rep.session is not None
 
-    async def test_add_user_return_user_model_with_username_and_id(self):
+    @pytest.mark.anyio
+    async def test_add_user_return_user_model_with_username_and_id(self, session):
         """ Тест добавления пользователя """
 
-        user_rep = UserRepository(self.session)
+        user_rep = UserRepository(session)
 
         user_data = {'username': 'test_username',
                      'password': 'test_password'}
-        user_data_from_db = await user_rep.add_one(user_data)
+        user_from_db = await user_rep.add_one(user_data)
+        user_data_from_db = UserFromDB.model_validate(user_from_db)
 
-        self.assertEqual(
-            user_data['username'],
-            user_data_from_db.username
-        )
-        self.assertIsNotNone(
-            user_data_from_db.id
-        )
+        await session.close()
+
+        assert user_data_from_db.username == user_data['username']
+
+    @pytest.mark.anyio
+    async def test_add_user_return_user_model_with_id(self, session):
+        """ Тест добавления пользователя """
+
+        user_rep = UserRepository(session)
+
+        user_data = {'username': 'test_username',
+                     'password': 'test_password'}
+        user_from_db = await user_rep.add_one(user_data)
+        user_data_from_db = UserFromDB.model_validate(user_from_db)
+
+        await session.close()
+
+        assert user_data_from_db.id is not None
